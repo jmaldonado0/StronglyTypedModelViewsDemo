40c72d95b484af92b3ba6bb7e5ac8f46959c8b50
jmaldonado0 <55056777+jmaldonado0@users.noreply.github.com>
2021-08-31 17:43:38 -0400

    Initial commit

---
+++ StronglyTypedModelViewsDemo/Controllers/ProductsController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StronglyTypedModelViewsDemo.Models;
+
+namespace StronglyTypedModelViewsDemo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        static List<Product> _Products = new List<Product>
+        {
+            new Product
+            {
+                ProductID = 1,
+                ProductName = "AMD Ryzen 3990",
+                Quantity = 100,
+                UnitInStock = 50,
+                Discontinued = false,
+                Cost = 3000,
+                LaunchDate = new DateTime(2021,10,1)
+            },
+            new Product
+            {
+                 ProductID = 2,
+                ProductName = "AMD Ryzen 3970",
+                Quantity = 100,
+                UnitInStock = 70,
+                Discontinued = false,
+                Cost = 2500,
+                LaunchDate = new DateTime(2021,10,5)
+            },
+            new Product
+            {
+                 ProductID =3,
+                ProductName = "AMD Ryzen 3960",
+                Quantity = 100,
+                UnitInStock = 80,
+                Discontinued = false,
+                Cost = 2000,
+                LaunchDate = new DateTime(29,10,15)
+            },
+
+            new Product
+            {
+                 ProductID = 4,
+                ProductName = "AMD Ryzen 3950",
+                Quantity = 100,
+                UnitInStock = 40,
+                Discontinued = false,
+                Cost = 1500,
+                LaunchDate = new DateTime(2021,10,25)
+            }
+        };
+         public IActionResult Index()
+        {
+
+
+            return View(_Products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var prod = _Products.Find(prod => prod.ProductID.Equals(id));
+            return View(prod);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var prod = _Products.Find(prod => prod.ProductID.Equals(id));
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product modifiedproduct)
+        {
+            var prod = _Products.FirstOrDefault( prod => prod.ProductID.Equals(modifiedproduct.ProductID));
+            var indexOf = _Products.IndexOf(prod);
+            modifiedproduct.Tax = modifiedproduct.Cost * 10 / 100;
+
+            _Products[indexOf] = modifiedproduct;
+
+          return View("Index", _Products);
+
+            
+            //return View(modifiedproduct);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var prod = _Products.Find(prod => prod.ProductID.Equals(id));
+            return View(prod);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var prod = _Products.Find(prod => prod.ProductID.Equals(id));
+            _Products.Remove(prod);
+            return View("Index", _Products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product NewProduct)
+        {
+            if(ModelState.IsValid)
+            {
+                NewProduct.Tax = NewProduct.Cost * 10 / 100;
+                _Products.Add(NewProduct);
+                return View("Index", _Products);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ StronglyTypedModelViewsDemo/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StronglyTypedModelViewsDemo.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+
+        public int UnitInStock { get; set; }
+
+        public bool Discontinued { get; set; }
+
+        public double Cost { get; set; }
+
+        public double Tax { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+    }
+}
